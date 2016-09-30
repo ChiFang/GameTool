8a19015f21aaf1f3106307bff4287f7a7a2f25f7
ChiFang <hardman761110@gmail.com>
2016-09-30 08:42:26 +0800

    first draft

---
+++ App1_GAME/App1_GAME/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace App1_GAME
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(162, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(66, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Get Pos";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(23, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(55, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(23, 54);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(55, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Current";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(100, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Wanted";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(92, 52);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(55, 22);
+            this.textBox4.TabIndex = 9;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(92, 24);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(55, 22);
+            this.textBox5.TabIndex = 8;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(151, 137);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(121, 47);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "OFF";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            this.button5.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.button5.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.button5_PreviewKeyDown);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(166, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(25, 12);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Step";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(216, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 12);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Delay (ms)";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(151, 109);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(55, 22);
+            this.textBox6.TabIndex = 14;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(216, 109);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(55, 22);
+            this.textBox7.TabIndex = 15;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(23, 149);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Uninstal";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(31, 133);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(44, 12);
+            this.label6.TabIndex = 18;
+            this.label6.Text = "Hot Key";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 207);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.OnKeyDownHandler);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ App1_GAME/App1_GAME/Form1.cs
@@ -0,0 +1,380 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Threading;
+using System.Runtime.InteropServices;
+
+using System.Diagnostics;
+
+
+namespace App1_GAME
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //
+        private const bool GLOBAL_HOT_KEY = true;
+        Thread Thread_Test;
+        public const int DEFAULT_STEP = 100;
+        public const int DEFAULT_DELAY = 500;
+
+        private static bool m_AutoClick = false;    // Auto Click status
+
+        const int WH_KEYBOARD = 2;
+        const int WH_KEYBOARD_LL = 13;
+
+        public delegate int HookProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        private static int m_HookHandle = 0;    // Hook handle
+        private HookProc m_KbdHookProc;            // 鍵盤掛鉤函式指標
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        // 設置掛鉤.
+        [DllImport("user32.dll", CharSet = CharSet.Auto,
+        CallingConvention = CallingConvention.StdCall)]
+        public static extern int SetWindowsHookEx(int idHook, HookProc lpfn,
+        IntPtr hInstance, int threadId);
+
+        // 將之前設置的掛鉤移除。記得在應用程式結束前呼叫此函式.
+        [DllImport("user32.dll", CharSet = CharSet.Auto,
+        CallingConvention = CallingConvention.StdCall)]
+        public static extern bool UnhookWindowsHookEx(int idHook);
+
+        // 呼叫下一個掛鉤處理常式（若不這麼做，會令其他掛鉤處理常式失效）.
+        [DllImport("user32.dll", CharSet = CharSet.Auto,
+        CallingConvention = CallingConvention.StdCall)]
+        public static extern int CallNextHookEx(int idHook, int nCode,
+        IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll")]
+        static extern int GetCurrentThreadId();
+
+        //
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GetMousePos();
+        }
+
+        public void GetMousePos()
+        {
+            // claer first and show position
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox1.AppendText("x:" + System.Windows.Forms.Cursor.Position.X.ToString());
+            textBox2.AppendText("y:" + System.Windows.Forms.Cursor.Position.Y.ToString());
+
+            // use as default position
+            // claer first and show position
+            textBox5.Clear();
+            textBox4.Clear();
+            textBox5.AppendText(System.Windows.Forms.Cursor.Position.X.ToString());
+            textBox4.AppendText(System.Windows.Forms.Cursor.Position.Y.ToString());
+
+            // SET default step and delay
+            textBox6.Clear();
+            textBox6.AppendText(DEFAULT_STEP.ToString());
+            textBox7.Clear();
+            textBox7.AppendText(DEFAULT_DELAY.ToString());
+        }
+
+        private void Move_Mouse(int a_lX, int a_lY)
+        {
+            Cursor.Position = new Point(a_lX, a_lY);
+        }
+
+        private void OnKeyDownHandler(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.ShiftKey)
+            {   // Start auto click
+                Hot_Key_Install();
+                AutoClick_Switch();
+            }
+
+            if (e.KeyCode == Keys.Enter)
+            {   // Get Mouse's Position
+                GetMousePos();
+            }
+
+            if (e.KeyCode == Keys.ControlKey)
+            {   // Hot Key Switch
+                Hot_Key_Switch();
+            }
+        }
+
+        private void AutoClick()
+        {
+            int lx = 0, ly = 0, lcnt = 0;
+            int ltmpx = 0, ltmpy = 0, lStep = 0;
+            int lTimeGap = 1500;
+
+            lx = int.Parse(textBox5.Text);
+            ly = int.Parse(textBox4.Text);
+            lStep = int.Parse(textBox6.Text);
+            lTimeGap = int.Parse(textBox7.Text);
+
+            while (lcnt < 10)
+            {
+                ltmpx = lx + lStep;
+                ltmpy = ly;
+                Move_Mouse(ltmpx, ltmpy);
+                Mouse.LeftClick();
+                Thread.Sleep(lTimeGap); //Delay
+
+                ltmpx = lx - lStep;
+                ltmpy = ly;
+                Move_Mouse(ltmpx, ltmpy);
+                Mouse.LeftClick();
+                Thread.Sleep(lTimeGap); //Delay
+
+                // lcnt++;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            AutoClick_Switch();
+        }
+
+        private void AutoClick_Switch()
+        {
+            if (m_AutoClick == false)
+            {   
+                StartAutoClick();
+            }
+            else
+            {
+                StopAutoClick();
+            }
+        }
+
+        private void StartAutoClick()
+        {
+            Thread_Test = new Thread(AutoClick);
+
+            button5.Text = "ON";
+            m_AutoClick = true;
+
+            Thread_Test.IsBackground = true;
+            Thread_Test.Start();
+        }
+
+        private void StopAutoClick()
+        {
+            if (Thread_Test != null && Thread_Test.IsAlive)
+            {
+                Thread_Test.Abort();
+                
+                button5.Text = "OFF";
+                m_AutoClick = false;
+            }
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+        }
+
+        private void button5_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+           
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            Hot_Key_Switch();
+        }
+
+        private void Hot_Key_Switch()
+        {
+            if (m_HookHandle == 0)
+            {
+                Hot_Key_Install();
+            }
+            else
+            {
+                Hot_Key_Uninstall();
+            }
+        }
+
+        private void Hot_Key_Uninstall()
+        {
+            bool ret = UnhookWindowsHookEx(m_HookHandle);
+            if (ret == false)
+            {
+                MessageBox.Show("呼叫 UnhookWindowsHookEx 失敗!");
+                return;
+            }
+            m_HookHandle = 0;
+            button2.Text = "Install";
+        }
+
+        private void Hot_Key_Install()
+        {
+            if (GLOBAL_HOT_KEY == true)
+            {
+                using (Process curProcess = Process.GetCurrentProcess())
+                using (ProcessModule curModule = curProcess.MainModule)
+                {
+                    m_KbdHookProc = new HookProc(KeyboardHookProc);
+                    m_HookHandle = SetWindowsHookEx(WH_KEYBOARD_LL, m_KbdHookProc, GetModuleHandle(curModule.ModuleName), 0);
+                }
+            }
+            else
+            {
+                m_KbdHookProc = new HookProc(KeyboardHookProc);
+                m_HookHandle = SetWindowsHookEx(WH_KEYBOARD, m_KbdHookProc, IntPtr.Zero, GetCurrentThreadId());
+            }
+
+
+            if (m_HookHandle == 0)
+            {
+                MessageBox.Show("呼叫 SetWindowsHookEx 失敗!");
+                return;
+            }
+            button2.Text = "Uninstall";
+        }
+
+        public int KeyboardHookProc(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            // 當按鍵按下及鬆開時都會觸發此函式，這裡只處理鍵盤按下的情形。
+
+            bool isPressed = (lParam.ToInt32() & 0x80000000) == 0;
+
+            if (nCode < 0 || !isPressed)
+            {
+                return CallNextHookEx(m_HookHandle, nCode, wParam, lParam);
+            }
+
+            // 取得欲攔截之按鍵狀態
+            KeyStateInfo ctrlKey = KeyboardInfo.GetKeyState(Keys.ControlKey);
+            KeyStateInfo altKey = KeyboardInfo.GetKeyState(Keys.Alt);
+            KeyStateInfo shiftKey = KeyboardInfo.GetKeyState(Keys.ShiftKey);
+            KeyStateInfo f8Key = KeyboardInfo.GetKeyState(Keys.F8);
+
+            if (ctrlKey.IsPressed)
+            {
+                System.Diagnostics.Debug.WriteLine("Ctrl Pressed!");
+            }
+            if (altKey.IsPressed)
+            {
+                System.Diagnostics.Debug.WriteLine("Alt Pressed!");
+                AutoClick_Switch();
+                Hot_Key_Uninstall();
+            }
+            if (shiftKey.IsPressed)
+            {
+                System.Diagnostics.Debug.WriteLine("Shift Pressed!");
+            }
+            if (f8Key.IsPressed)
+            {
+                System.Diagnostics.Debug.WriteLine("F8 Pressed!");
+            }
+
+            return CallNextHookEx(m_HookHandle, nCode, wParam, lParam);
+        }
+    }
+
+    public class KeyboardInfo
+    {
+        private KeyboardInfo() { }
+
+        [DllImport("user32")]
+        private static extern short GetKeyState(int vKey);
+
+        public static KeyStateInfo GetKeyState(Keys key)
+        {
+            int vkey = (int)key;
+
+            if (key == Keys.Alt)
+            {
+                vkey = 0x12;    // VK_ALT
+            }
+
+            short keyState = GetKeyState(vkey);
+            int low = Low(keyState);
+            int high = High(keyState);
+            bool toggled = (low == 1);
+            bool pressed = (high == 1);
+
+            return new KeyStateInfo(key, pressed, toggled);
+        }
+
+        private static int High(int keyState)
+        {
+            if (keyState > 0)
+            {
+                return keyState >> 0x10;
+            }
+            else
+            {
+                return (keyState >> 0x10) & 0x1;
+            }
+
+        }
+
+        private static int Low(int keyState)
+        {
+            return keyState & 0xffff;
+        }
+    }
+
+    public struct KeyStateInfo
+    {
+        Keys m_Key;
+        bool m_IsPressed;
+        bool m_IsToggled;
+
+        public KeyStateInfo(Keys key, bool ispressed, bool istoggled)
+        {
+            m_Key = key;
+            m_IsPressed = ispressed;
+            m_IsToggled = istoggled;
+        }
+
+        public static KeyStateInfo Default
+        {
+            get
+            {
+                return new KeyStateInfo(Keys.None, false, false);
+            }
+        }
+
+        public Keys Key
+        {
+            get { return m_Key; }
+        }
+
+        public bool IsPressed
+        {
+            get { return m_IsPressed; }
+        }
+
+        public bool IsToggled
+        {
+            get { return m_IsToggled; }
+        }
+    }
+
+
+
+
+}
